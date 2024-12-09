d5a093e3f5379ce91d1e45f2dda2caf57a154fac
BobyDev <117381532+Bobyvivek@users.noreply.github.com>
2024-12-09 19:09:57 +0530

    Update Main.php

--- src/BDWalcomeMsg/BobyDev/Main.php
+++ src/BDWalcomeMsg/BobyDev/Main.php
@@ -31,13 +31,13 @@ GitHub: https://github.com/Bobyvivek");
 
         // Header message
         $player->sendMessage(TextFormat::GRAY . str_repeat("-", 50));
-        $player->sendMessage(TextFormat::GOLD . "Welcome to " . TextFormat::LIGHT_PURPLE . "RiftMC");
+        $player->sendMessage(TextFormat::GOLD . "Welcome to " . TextFormat::LIGHT_PURPLE . "ServerName");
 
         // Section: Store, Website, Discord, Twitter
-        $player->sendMessage(TextFormat::RED . "STORE: " . TextFormat::YELLOW . "shop.riftmc.co " . TextFormat::GRAY . "[50% OFF SALE]");
-        $player->sendMessage(TextFormat::YELLOW . "WEBSITE: " . TextFormat::WHITE . "www.riftmc.co");
-        $player->sendMessage(TextFormat::AQUA . "DISCORD: " . TextFormat::WHITE . "riftmc.co/discord");
-        $player->sendMessage(TextFormat::DARK_AQUA . "TWITTER: " . TextFormat::WHITE . "@riftmc_co");
+        $player->sendMessage(TextFormat::RED . "STORE: " . TextFormat::YELLOW . "shopName " . TextFormat::GRAY . "[50% OFF SALE]");
+        $player->sendMessage(TextFormat::YELLOW . "WEBSITE: " . TextFormat::WHITE . "Web name ");
+        $player->sendMessage(TextFormat::AQUA . "DISCORD: " . TextFormat::WHITE . "discord link ");
+        $player->sendMessage(TextFormat::DARK_AQUA . "TWITTER: " . TextFormat::WHITE . "@Name");
 
         // Footer message
         $player->sendMessage(TextFormat::LIGHT_PURPLE . "UPDATES: " . TextFormat::GRAY . "You have unread updates. Use /updates to view.");
